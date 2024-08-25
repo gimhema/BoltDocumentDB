ba3b0c73f7c3ba65b5340515269cd51f1152529c
kimmo <hemakim0215@naver.com>
2024-08-25 10:26:46 +0900

    write crud

--- BoltDB/src/data/document.rs
+++ BoltDB/src/data/document.rs
@@ -33,11 +33,44 @@ impl BoltDocument {
     }
 
     pub fn update_object(&mut self, key : i64, query : String) {
-        
+        if let Some(existing_obj) = self.document.get_mut(&key) {
+            let parsed_result: Result<Value, Error> = serde_json::from_str(&query);
+
+            match parsed_result {
+                Ok(new_data) => {
+                    // Update the existing object with the new data
+                    BoltDocument::merge_json(&mut existing_obj.data, &new_data);
+                }
+                Err(e) => {
+                    println!("Failed to parse JSON: {}", e);
+                }
+            }
+        } else {
+            println!("No document found with the key: {}", key);
+        }
     }
 
     pub fn delete_object(&mut self, key : i64) {
         self.document.remove(&key);
+    }
+
+    fn merge_json(existing: &mut Value, updates: &Value) {
+        if let (Value::Object(existing_map), Value::Object(updates_map)) = (existing, updates) {
+            for (key, value) in updates_map {
+                match value {
+                    Value::Object(_) => {
+                        if let Some(existing_value) = existing_map.get_mut(key) {
+                            BoltDocument::merge_json(existing_value, value);
+                        } else {
+                            existing_map.insert(key.clone(), value.clone());
+                        }
+                    }
+                    _ => {
+                        existing_map.insert(key.clone(), value.clone());
+                    }
+                }
+            }
+        }
     }
 }
 
