7051115049ec98e55108c394049c6a592c481d15
kimmo <hemakim0215@naver.com>
2024-09-05 11:18:35 +0900

    .

--- BoltDB/src/agent/query.rs
+++ BoltDB/src/agent/query.rs
@@ -12,6 +12,40 @@
 // update <BUNDLE_NALE> <DOCMUENT_NAME> <KEY> <JSON_QUERY>
 // delete <BUNDLE_NALE> <DOCMUENT_NAME> <KEY>
 
+pub enum QueryType {
+    NONE,
+    NEW,
+    RESTORE,
+    BACKUP,
+    REMOVE,
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    END
+}
+
+pub struct QueryResult {
+    query_type : QueryType,
+    bundle_name : String,
+    document_name : String,
+    key : i64,
+    json_query : String
+}
+
+impl QueryResult {
+    pub fn new(
+        _query_type : QueryType,
+        _bundle_name : String,
+        _document_name : String,
+        _key : i64,
+        _json_query : String) -> Self {
+            return QueryResult { 
+                query_type: _query_type, bundle_name: _bundle_name,
+                 document_name: _document_name, key: _key, json_query: _json_query }
+        }
+}
+
 pub struct BoltInterpreter {
     input_query : String
 }
