152bd473c04a4f0e7bcf668bdb6b2c14f8d74d90
kimmo <hemakim0215@naver.com>
2024-09-18 10:15:35 +0900

    .

--- BoltDB/src/main.rs
+++ BoltDB/src/main.rs
@@ -1,16 +1,29 @@
+#[macro_use]
+extern crate lazy_static;
 
 mod data;
 mod connection;
 mod agent;
-
 use agent::bolt_agent::*;
+
+use std::sync::{RwLock, Arc};
+
+lazy_static! {
+    static ref BOLT_AGENT_INSTANCE: Arc<RwLock<BoltAgent>> = Arc::new(RwLock::new(BoltAgent::new()));
+}
+
+impl BoltAgent {
+    pub fn get_instance() -> &'static Arc<RwLock<BoltAgent>> {
+        &BOLT_AGENT_INSTANCE
+    }
+}
 
 fn main() {
     
     print!("Start Bolt DB ");
 
-    let mut bolt_agent = BoltAgent::new();
-
-    bolt_agent.wait();
+    // let mut bolt_agent = BoltAgent::new();
+    // bolt_agent.wait();
+    BoltAgent::get_instance().write().unwrap().wait();
 
 }
